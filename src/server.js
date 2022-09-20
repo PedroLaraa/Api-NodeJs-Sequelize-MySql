a0dddaca209f8885af1732393ca9e83525c20816
Pedro Lara <pedroalveslara@gmail.com>
2022-09-20 14:02:16 -0300

    Package installed

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3030;
+
+app.use(bodyParser.json()); // Avisa o Express que o body parser vai pegar os dados e converter em JSON
+
+app.get('/teste', (req, res) => {
+    res.status(200).send({message: 'Funcionando!'});
+})
+
+app.listen(port, () => {
+    console.log('Server rodando na porta:', port);
+});
